27f6615a45d14580e65654e295d777ed1da65a4a
Sjoerd van den Dries <sdries@sdries.(none)>
2014-03-26 23:05:53 +0100

    Started ai interface

---
+++ include/AICommand.h
@@ -0,0 +1,13 @@
+#pragma once
+
+class AICommand {
+
+public:
+
+    AICommand();
+
+    virtual ~AICommand();
+
+};
+
+#endif
---
+++ include/ai/IAI.h
@@ -0,0 +1,20 @@
+#pragma once
+
+class AIInput;
+class AICommand;
+
+class IAI
+{
+
+public:
+
+    IAI();
+
+    virtual ~IAI();
+
+    // Update hook called every frame
+    void update(const AIInput& input, AICommand& command);
+
+};
+
+#endif
