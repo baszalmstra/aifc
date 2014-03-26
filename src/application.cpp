f028a6012f1ce6d6526d0f7781c442d6179b9ba2
Bas Zalmstra <zalmstra.bas@gmail.com>
2014-03-26 22:51:41 +0100

    no message

--- src/application.cpp
+++ src/application.cpp
@@ -17,18 +17,31 @@ Application::~Application()
 //-------------------------------------------------------------------------------------------------
 int Application::Run()
 {
+  // Initialize the application
+  if (!Initialize())
+    return -1;
 
-  return 0;
+
+  // Tear down the application
+  return Destroy() ? 0 : -1;
 }
 
 //-------------------------------------------------------------------------------------------------
 bool Application::Initialize()
 {
+  // Initialize SDL
+  if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
+  {
+    SDL_LogError(SDL_LOG_CATEGORY_SYSTEM, "Unable to initialize SDL: %s", SDL_GetError());
+    return false;
+  }
+
   return true;
 }
 
 //-------------------------------------------------------------------------------------------------
 bool Application::Destroy()
 {
+  SDL_Quit();
   return true;
 }
